9cecf23b6be7a00bdbb7783553bd785497c6fd54
Tomas Stolker <stolker@strw.leidenuniv.nl>
2021-07-29 14:11:05 +0200

    v0.1.2

--- diskmap/__init__.py
+++ diskmap/__init__.py
@@ -2,7 +2,7 @@ from diskmap.diskmap import DiskMap
 
 __author__ = 'Tomas Stolker'
 __license__ = 'MIT'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __maintainer__ = 'Tomas Stolker'
 __email__ = 'stolker@strw.leidenuniv.nl'
 __status__ = 'Development'
